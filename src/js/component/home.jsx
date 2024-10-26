4067594fa73ebadd73fd38cab731e80fa9395ec8
Edvin Johannes Piirsalu <edvin.piirsalu@gmail.com>
2024-10-26 10:28:34 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	
+	const [color, setColor] = useState("");
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="wrapper">
+
+		<div className="palo container d-flex flex-column justify-content-center align-items-center"></div>
+
+		<div className="semaforo bg-dark container d-flex flex-column justify-content-center align-items-center">
+		<button onClick={()=>setColor('rojo')} className={`btn-rojo bg-danger p-2 ${color=== "rojo" ? "brillandoRojo" : ""}`}></button>
+		<button onClick={()=>setColor('amarillo')} className={`btn-amarillo bg-warning p-2 ${color === "amarillo" ? "brillandoAmarillo" : ""}`}></button>
+		<button onClick={()=>setColor('verde')} className={`btn-verde bg-success p-2 ${color === "verde"? "brillandoVerde" : ""}`}></button>
 		</div>
+		
+		<div className="mt-5 d-flex justify-content-center align-items-center">
+		<button onClick={()=>setColor("")} className="bg-primary text-white fs-3 rounded ">Apagar</button>
+		</div>
+		
+		</div>
+
 	);
 };
 
